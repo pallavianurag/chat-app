b72b77414767bdd85909dfa01cabee56df0a6ed2
pallavianurag <anuragpallavi999@gmail.com>
2021-06-28 19:16:11 +0530

    initialised firebase project

---
+++ projects/chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const config={
+    apiKey: "AIzaSyCbLsLI0uFO3ITRTvL1Do6ZlCVvlrRC8OQ",
+    authDomain: "chat-web-app-2abaa.firebaseapp.com",
+    projectId: "chat-web-app-2abaa",
+    storageBucket: "chat-web-app-2abaa.appspot.com",
+    messagingSenderId: "29404721089",
+    appId: "1:29404721089:web:2a210bb729b2f1242a0940"
+  };
+
+  const app=firebase.initializeApp(config);
